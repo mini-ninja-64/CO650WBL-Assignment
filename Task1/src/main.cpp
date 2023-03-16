5d68fdef1f62332cd986023d6be5b52535b8e120
Callum Hart <github@callumh.art>
2023-03-16 20:41:14 +0000

    Add basic project structure

---
+++ Task1/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#ifndef TESTING
+int main() {
+    std::cout << "hello world" << std::endl;
+}
+#endif
---
+++ Task1/test/SimpleTest.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(SimpleTest, GoogleTestIsWorking) {
+    EXPECT_EQ(0, 0);
+}
