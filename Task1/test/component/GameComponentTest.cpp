120fe8a31456f4f72b43396fd7155c2d29e17af4
mini-ninja-64 <github@callumh.art>
2023-03-17 19:08:12 +0000

    Update GameComponentTest to check count offset

--- Task1/test/component/GameComponentTest.cpp
+++ Task1/test/component/GameComponentTest.cpp
@@ -5,14 +5,16 @@
 #include "component/GameComponent.hpp"
 
 TEST(GameComponentTest, IncrementsInstancesOnInstantiationAndUsesItForID) {
+    const int initialInstanceCount = GameComponent::getInstances();
+
     GameComponent gameComponent1;
     auto* gameComponent2 = new GameComponent();
     auto gameComponent3 = std::make_unique<GameComponent>();
 
-    EXPECT_EQ(gameComponent1.getId(), 1);
-    EXPECT_EQ(gameComponent2->getId(), 2);
-    EXPECT_EQ(gameComponent3->getId(), 3);
-    EXPECT_EQ(GameComponent::getInstances(), 3);
+    EXPECT_EQ(gameComponent1.getId(), initialInstanceCount+1);
+    EXPECT_EQ(gameComponent2->getId(), initialInstanceCount+2);
+    EXPECT_EQ(gameComponent3->getId(), initialInstanceCount+3);
+    EXPECT_EQ(GameComponent::getInstances(), initialInstanceCount+3);
 
     delete gameComponent2;
 }
